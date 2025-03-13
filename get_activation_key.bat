8d6e2971447f1b8856eca295265ffc8b7c5a9901
Paul Smith <paul@bitdog.me>
2025-03-13 06:30:39 -0600

    cleanup output

--- get_activation_key.bat
+++ get_activation_key.bat
@@ -1,3 +1,5 @@
 @echo off
+rem reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" /v BackupProductKeyDefault
+
 rem Not sure this always works
-reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" /v BackupProductKeyDefault
+reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" /v BackupProductKeyDefault > out && findstr/c:REG_SZ out > out2 && set /p key= < out2 && del out && del out2 && echo %key%
